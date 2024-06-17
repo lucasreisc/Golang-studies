1a06a065333010f77cc980768dd55eb219e9c73b
Lucas Reis <66570455+lucasreisc@users.noreply.github.com>
2024-06-16 22:18:11 -0300

    studies
    studies

---
+++ 2-Variaveis/variaveis.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var variavel1 string = "variavel 1"
+	variavel2 := "Variavel 2"
+	fmt.Println(variavel1)
+	fmt.Println(variavel2)
+
+	var (
+		variavel3 string = "lalala"
+		variavel4 string = "lalala"
+
+	
+	)
+
+	fmt.Println(variavel3 , variavel4)
+	
+}
